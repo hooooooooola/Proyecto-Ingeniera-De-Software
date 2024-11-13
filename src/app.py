7bc8adbe0670d9d339fd1a6caae9b4a07c765e12
hooooooooola <167253637+hooooooooola@users.noreply.github.com>
2024-11-13 20:49:45 +0000

    feat: despliegue de medicos en vista Especialistas

--- src/app.py
+++ src/app.py
@@ -6,9 +6,19 @@ app = Flask(__name__)
 def inicio():
     return render_template('inicio.html')
 
-@app.route('/servicios')
-def servicios():
-    return render_template('servicios.html')
+@app.route('/especialistas')
+def especialistas():
+    # conectar esta wea a la base de datos
+    especialistas_data = [
+        {'nombre': 'Adalis Amanda Rodríguez', 'especialidad': 'Cirugía Plástica', 'foto': 'medico3.png'},
+        {'nombre': 'Adolfo Cid Guzmán', 'especialidad': 'Medicina General', 'foto': 'medi1.jpg'},
+        {'nombre': 'Luis Pérez', 'especialidad': 'Cardiología', 'foto': 'foto2.jpg'},
+        {'nombre': 'María Rodríguez', 'especialidad': 'Pediatría', 'foto': 'foto3.jpg'},
+        {'nombre': 'Juan Pérez', 'especialidad': 'Ginecología', 'foto': 'foto4.jpg'},
+        {'nombre': 'Juan Pérez', 'especialidad': 'Ginecología', 'foto': 'foto4.jpg'},
+
+    ]
+    return render_template('especialistas.html', especialistas=especialistas_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
